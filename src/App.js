63e72e148526784b4d0f2d327e6553757b872f11
Fensley <fortilusnehemie@gmail.com>
2024-04-29 15:06:05 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,98 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  function handleValue(e) {
+    setValue(+e.target.value);
+  }
+
+  const [Second, setSecond] = useState(0);
+  function SecondHanle(e) {
+    setSecond(+e.target.value);
+  }
+  const math = (value * Second) / 100;
+  const [last, setlast] = useState(0);
+  function thirdHanle(e) {
+    setlast(e.target.value);
+  }
+  const math2 = (value * last) / 100;
+  let Final = math + math2;
+
   return (
     <div>
-      <Bill />
+      <First value={value} handleValue={handleValue} />
+      <SecondF value={value} SecondHanle={SecondHanle} math={math} />
+      <Third
+        value={value}
+        SecondHanle={SecondHanle}
+        math={math}
+        thirdHanle={thirdHanle}
+        Final={Final}
+      />
+      <Fouth
+        value={value}
+        SecondHanle={SecondHanle}
+        math={math}
+        math2={math2}
+        Final={Final}
+      />
+      <Five value={value} SecondHanle={SecondHanle} math={math} />
     </div>
   );
 }
 
-function Bill() {
+function First({ value, handleValue, SecondHanle, math }) {
   return (
     <div>
-      <span> How much was the Bill ? </span>
-      <input />
+      <span> How much was the Bill ?</span>
+      <input placeholder="cost of the food" onChange={handleValue} />
       <br />
-
+    </div>
+  );
+}
+function SecondF({ SecondHanle }) {
+  return (
+    <>
       <span> How did you like the service ? </span>
-      <select>
-        <option value="dd">Dissatisfied (0%)</option>
-        <option value="dd">It was okay (5%) </option>
-        <option value="dd"> It was good (10%) </option>
-        <option value="dd"> it was amazing (20%) </option>
+      <select onChange={SecondHanle}>
+        <option value={0}>Dissatisfied (0%) </option>
+        <option value={5}>It was okay (5%) </option>
+        <option value={10}> It was good (10%) </option>
+        <option value={20}> it was amazing (20%) </option>
       </select>
-
+    </>
+  );
+}
+function Third({ SecondHanle, thirdHanle }) {
+  return (
+    <>
       <br />
       <span> How did your friend like the service ? </span>
-      <select>
-        <option value="dd"> Dissatisfied (0%)</option>
-        <option value="dd">It was okay (5%) </option>
-        <option value="dd"> It was good (10%) </option>
-        <option value="dd"> Absolutely amazing! (20%) </option>
+      <select onChange={thirdHanle}>
+        <option value={0}> Dissatisfied (0%)</option>
+
+        <option value={5}>It was okay (5%) </option>
+
+        <option value={10}> It was good (10%) </option>
+
+        <option value={20}> Absolutely amazing! (20%) </option>
       </select>
-
-      <h1> You pay $100 ($100 + $0 tip)</h1>
-
-      <button> Reset</button>
-    </div>
+    </>
   );
 }
 
+function Fouth({ value, Final }) {
+  return (
+    <h1>
+      {" "}
+      You pay $ {value === "" ? 0 : value + Final} ($
+      {value === "" ? 0 : value} + $ {Final}
+      tip)
+    </h1>
+  );
+}
+function Five({ reset }) {
+  return <button> Reset</button>;
+}
 export default App;
